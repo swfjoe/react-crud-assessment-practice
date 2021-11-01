f40e8eaa0f83c9684c990a57e26cf563b3bba900
Joseph Myrick <joseph.l.myrick2.mil@swf.army.mil>
2021-11-01 09:25:57 -0500

    Initial build.

---
+++ src/main/java/com/example/reactcrudassessmentpractice/ReactCrudAssessmentPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactcrudassessmentpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactCrudAssessmentPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactCrudAssessmentPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactcrudassessmentpractice/ReactCrudAssessmentPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactcrudassessmentpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactCrudAssessmentPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
